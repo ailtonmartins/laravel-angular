10e8fa0a824a1d005875e05f1ccef8d6bf6a7b2c
Ailton Martins <pinho123@gmail.com>
2018-09-24 22:17:57 -0300

    Finalizado o crud de task

--- api/app/Http/Controllers/TaskController.php
+++ api/app/Http/Controllers/TaskController.php
@@ -8,28 +8,58 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $tasks = Tasks::get();
+        $tasks = Tasks::paginate(5);
         return response()->json($tasks);
     }
 
     public function show($id)
     {
-      
+        $task = Tasks::find($id);
+
+        if(!$task) {
+            return response()->json([
+                'message'   => 'Record not found',
+            ], 404);
+        }
+
+        return response()->json($task);
     }
 
     public function store(Request $request)
     {
+        $task= new Tasks();
+        $task->fill($request->all());
+        $task->save();
+
+        return response()->json($task, 201);
     }
 
     public function update(Request $request, $id)
     {
-        
+        $task = Tasks::find($id);
+
+        if(!$task) {
+            return response()->json([
+                'message'   => 'Record not found',
+            ], 404);
+        }
+
+        $task->fill($request->all());
+        $task->save();
+
+        return response()->json($task);
     }
 
     public function destroy($id)
     {
-       
+        $task = Tasks::find($id);
+        if(!$task) {
+            return response()->json([
+                'message'   => 'Record not found',
+            ], 404);
+        }
+        $task->delete();
     }
 }
--- api/app/Models/Tasks.php
+++ api/app/Models/Tasks.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Tasks extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = ['titulo', 'status', 'descricao'];
     protected $dates = ['deleted_at'];
 }
