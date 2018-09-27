a89df5e56b0a3476de655f915124dc98cea8e44c
Ailton Martins <pinho123@gmail.com>
2018-09-26 23:23:33 -0300

    Instalado o cors

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,19 +1,5 @@
 <?php
 date_default_timezone_set('America/Sao_Paulo');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
-header('Access-Control-Allow-Credentials: true');
-
-Route::group(array('prefix' => 'api'), function()
-{
-
-  Route::get('/', function () {
-      return response()->json(['message' => 'TASK API', 'status' => 'Connected']);;
-  });
-
-  Route::resource('task', 'TaskController');
-});
 
 Route::get('/', function () {
     return redirect('api');
